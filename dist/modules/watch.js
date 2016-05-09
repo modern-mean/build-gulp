a29b6f0689413a7f6561524dafe0d7335009173b
Andrew Throener <athroener@gmail.com>
2016-05-08 21:12:41 -0500

    feat(modules): Watch working with main app

--- dist/modules/watch.js
+++ dist/modules/watch.js
@@ -13,12 +13,21 @@ var _child_process = require('child_process');
 
 var _client = require('./client');
 
+var clientBuild = _interopRequireWildcard(_client);
+
 var _server = require('./server');
+
+function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } else { var newObj = {}; if (obj != null) { for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) newObj[key] = obj[key]; } } newObj.default = obj; return newObj; } }
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
+function ok(done) {
+  console.log("WTF!!!!!!!!!!!!!!", clientBuild);
+  return done();
+}
+
 function client() {
-  return _gulp2.default.watch(['./src/client/**/*', '!**/*.constants.js', '!**/*.values.js'], _gulp2.default.series(_client.build));
+  return _gulp2.default.watch(['./src/client/**/*', '!**/*.constants.js', '!**/*.values.js'], _gulp2.default.series(ok, clientBuild.build));
 }
 client.displayName = 'modules:watch:client';
 
@@ -27,7 +36,18 @@ function server() {
 }
 server.displayName = 'modules:watch:server';
 
-let all = _gulp2.default.parallel(client, server);
+function send(done) {
+  if (process.send) {
+    process.send({ ready: true });
+    process.on('disconnect', () => {
+      //process.exit(1);
+      done();
+    });
+  }
+}
+send.displayName = 'modules:watch:ready';
+
+let all = _gulp2.default.parallel(client, server, send);
 all.displayName = 'modules:watch:all';
 
 exports.client = client;
--- src/modules/watch.js
+++ src/modules/watch.js
@@ -2,12 +2,17 @@
 
 import gulp from 'gulp';
 import { exec } from 'child_process';
-import { build as buildClient } from './client';
+import * as clientBuild from './client';
 import { build as buildServer } from './server';
+
+function ok(done) {
+  console.log("WTF!!!!!!!!!!!!!!", clientBuild)
+  return done();
+}
 
 
 function client() {
-  return gulp.watch(['./src/client/**/*', '!**/*.constants.js', '!**/*.values.js'], gulp.series(buildClient));
+  return gulp.watch(['./src/client/**/*', '!**/*.constants.js', '!**/*.values.js'], gulp.series(ok, clientBuild.build));
 }
 client.displayName = 'modules:watch:client';
 
@@ -16,9 +21,18 @@ function server() {
 }
 server.displayName = 'modules:watch:server';
 
+function send(done) {
+  if (process.send) {
+    process.send({ ready: true });
+    process.on('disconnect', () => {
+      //process.exit(1);
+      done();
+    });
+  }
+}
+send.displayName = 'modules:watch:ready';
 
-
-let all = gulp.parallel(client, server);
+let all = gulp.parallel(client, server, send);
 all.displayName = 'modules:watch:all';
 
 export { client, server, all };
