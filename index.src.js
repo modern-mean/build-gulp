007d139b643dd5fa88fe68426cd0bbe05fbea29e
Andrew Throener <athroener@gmail.com>
2016-08-18 12:12:15 -0500

    Removed Release

--- index.src.js
+++ index.src.js
@@ -5,10 +5,9 @@ import * as build from './dist/build';
 import * as test from './dist/test';
 import * as lint from './dist/lint';
 import * as watch from './dist/watch';
-import * as release from './dist/release';
 
 let clean = gulp.parallel(build.clean, test.clean);
 clean.displayName = 'modules:clean';
 gulp.task(clean);
 
-export { build, test, lint, clean, watch, release };
+export { build, test, lint, clean, watch };
