945835ace5536aa0084ad6e62a01f75b9352c82d
Andrew Throener <athroener@gmail.com>
2016-08-25 13:55:09 -0500

    fixes

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,27 +1,20 @@
 'use strict';
 
 import gulp from 'gulp';
-import babel from 'gulp-babel';
-import del from 'del';
-import filter from 'gulp-filter';
-import rename from 'gulp-rename';
-import * as build from './src/build.es6';
-import * as lint from './src/lint.es6';
+import * as builder from './src/build.es6';
+import * as linter from './src/lint.es6';
 
-function clean() {
-  return del([
-    './dist'
-  ]);
-}
-clean.displayName = 'clean';
-gulp.task(clean);
+//gulp build
+let build =  gulp.series(builder.clean, builder.all);
+build.displayName = 'build';
+gulp.task(build);
 
-//Gulp Default
-//let defaultTask = gulp.series(modules.clean, modules.server.config, gulp.parallel(modules.client.build, modules.server.build));
-let defaultTask = gulp.series(clean, build.all);
-defaultTask.displayName = 'default';
-gulp.task(defaultTask);
-
-let test = gulp.series(lint.all);
+//gulp test
+let test = gulp.series(linter.all);
 test.displayName = 'test';
 gulp.task(test);
+
+//gulp
+let defaultTask = gulp.series(build);
+defaultTask.displayName = 'default';
+gulp.task(defaultTask);
